7aa7fedc63ea0af2e90739b500077d2eaa35516b
Matthew-Henderson <2301614@uad.ac.uk>
2025-12-18 15:51:55 +0000

    Added Default scene

---
+++ CMP304Project/Source/CMP304Project/CMP304Project.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CMP304Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CMP304Project, "CMP304Project" );
